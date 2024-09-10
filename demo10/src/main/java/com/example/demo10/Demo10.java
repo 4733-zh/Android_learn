d81aaff2a18c7fd228c57a488ebce3c5bdb66d9f
4733zh <939502270@qq.com>
2024-09-10 14:40:11 +0800

    Demo12使用广播实现强制下线 Demo10实现本地广播

--- demo10/src/main/java/com/example/demo10/Demo10.java
+++ demo10/src/main/java/com/example/demo10/Demo10.java
@@ -17,48 +17,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class Demo10 extends AppCompatActivity {
     private IntentFilter intentFilter;
-    private NetworkChangeReceiver networkChangeReceiver;
+    //    private NetworkChangeReceiver networkChangeReceiver;
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("com.example.demo10.MY_BROADCAST");
-                intent.setComponent(new ComponentName("com.example.demo10","com.example.demo10.MyBroadcastReceiver"));
-
-                sendOrderedBroadcast(intent,null);
+//                Intent intent = new Intent("com.example.demo10.MY_BROADCAST");
+//                intent.setComponent(new ComponentName("com.example.demo10", "com.example.demo10.MyBroadcastReceiver"));
+//                sendOrderedBroadcast(intent, null);
+                Intent intent = new Intent("com.example.demo10.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
             }
         });
+         intentFilter = new IntentFilter();
+         intentFilter.addAction("com.example.demo10.LOCAL_BROADCAST");
+         localReceiver = new LocalReceiver();
+         localBroadcastManager.registerReceiver(localReceiver,intentFilter);
 
-        intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver, intentFilter);
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver, intentFilter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+//        unregisterReceiver(networkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 
-    class NetworkChangeReceiver extends BroadcastReceiver {
+//    class NetworkChangeReceiver extends BroadcastReceiver {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            ConnectivityManager connectionManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+//            if (networkInfo != null && networkInfo.isAvailable()) {
+//                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+
+    private class LocalReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            ConnectivityManager connectionManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isAvailable()) {
-                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
-            }
+            Toast.makeText(context,"received local broadcast",Toast.LENGTH_SHORT).show();
         }
     }
 }
