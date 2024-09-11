f01c5e5ef6bfcb31f9b500add13d84804ab63af0
4733zh <939502270@qq.com>
2024-09-11 13:38:39 +0800

    databasetest中使用SQLite创建与升级数据库

--- demo12/src/main/java/com/example/demo12/ui/login/LoginActivity.java
+++ demo12/src/main/java/com/example/demo12/ui/login/LoginActivity.java
@@ -49,7 +49,7 @@ public class LoginActivity extends BaseActivity {
         pref = PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit = findViewById(R.id.account);
         passwordEdit = findViewById(R.id.password);
-        rememberPass = (CheckBox)findViewById(R.id.remember_pass);
+        rememberPass = findViewById(R.id.remember_pass);
         login = findViewById(R.id.login);
         boolean isRemember = pref.getBoolean("remember_password",false);
         if (isRemember) {
@@ -59,27 +59,24 @@ public class LoginActivity extends BaseActivity {
             passwordEdit.setText(password);
             rememberPass.setChecked(true);
         }
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String account = accountEdit.getText().toString();
-                String password = passwordEdit.getText().toString();
-                if (account.equals("admin")&&password.equals("123456")){
-                    editor = pref.edit();
-                    if (rememberPass.isChecked()){
-                        editor.putBoolean("remember_password",true);
-                        editor.putString("account",account);
-                        editor.putString("password",password);
-                    }else {
-                        editor.clear();
-                    }
-                    editor.apply();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
+        login.setOnClickListener(view -> {
+            String account = accountEdit.getText().toString();
+            String password = passwordEdit.getText().toString();
+            if (account.equals("admin")&&password.equals("123456")){
+                editor = pref.edit();
+                if (rememberPass.isChecked()){
+                    editor.putBoolean("remember_password",true);
+                    editor.putString("account",account);
+                    editor.putString("password",password);
                 }else {
-                    Toast.makeText(LoginActivity.this,"account or password is invalid",Toast.LENGTH_SHORT).show();
+                    editor.clear();
                 }
+                editor.apply();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }else {
+                Toast.makeText(LoginActivity.this,"account or password is invalid",Toast.LENGTH_SHORT).show();
             }
         });
     }
