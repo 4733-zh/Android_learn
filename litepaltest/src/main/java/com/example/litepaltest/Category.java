3328a76f5fccf6e77711f4fab33a022fc9947491
4733zh <939502270@qq.com>
2024-09-11 16:07:06 +0800

    litepaltest中使用LitePal中操作数据库，配置，创建

---
+++ litepaltest/src/main/java/com/example/litepaltest/Category.java
@@ -0,0 +1,31 @@
+package com.example.litepaltest;
+
+public class Category {
+    private int id;
+    private String categoryName;
+    private int categoruCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoruCode() {
+        return categoruCode;
+    }
+
+    public void setCategoruCode(int categoruCode) {
+        this.categoruCode = categoruCode;
+    }
+}
