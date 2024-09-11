35775cb4618f69b14f23efb83af0db6adf1d2f77
4733zh <939502270@qq.com>
2024-09-11 09:46:35 +0800

    sharedperferencestest将数据存储到sharedperferencestest中

---
+++ sharedperferencestest/src/main/java/com/example/sharedperferencestest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sharedperferencestest;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button saveData = (Button) findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name","Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married",false);
+                editor.apply();
+            }
+        });
+    }
+}
