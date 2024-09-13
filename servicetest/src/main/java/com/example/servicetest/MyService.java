39db6af1660e7976c67cc4fa837891f0b22d2c90
4733zh <939502270@qq.com>
2024-09-13 11:34:44 +0800

    servicetest中前台服务按钮功能完善 servicebestpractice中初步编写下载示例

--- servicetest/src/main/java/com/example/servicetest/MyService.java
+++ servicetest/src/main/java/com/example/servicetest/MyService.java
@@ -1,10 +1,21 @@
 package com.example.servicetest;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
     public MyService() {
@@ -15,7 +26,7 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("MyService","onCreate executed");
+//        Log.d("MyService","onCreate executed");
 //        Intent intent = new Intent(this,MainActivity.class);
 //        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
 //        Notification notification = new NotificationCompat.Builder(this)
@@ -27,7 +38,48 @@ public class MyService extends Service {
 //                .setContentIntent(pi)
 //                .build();
 //        startForeground(1,notification);
+        startForeground();
     }
+
+    private void startForeground() {
+        String channelId = null;
+        // 8.0 以上需要特殊处理
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channelId = createNotificationChannel("kim.hsl", "ForegroundService");
+        } else {
+            channelId = "";
+        }
+        Log.d("MyService","onCreate executed");
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+        Notification notification = new NotificationCompat.Builder(this,channelId)
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1, notification);
+    }
+
+    /**
+     * 创建通知通道
+     * @param channelId
+     * @param channelName
+     * @return
+     */
+    @RequiresApi(Build.VERSION_CODES.O)
+    private String createNotificationChannel(String channelId, String channelName){
+        NotificationChannel chan = new NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager service = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        service.createNotificationChannel(chan);
+        return channelId;
+    }
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
